eb12d9d06999fe0baa0d30578461de9f11887137
kandarp dangi <kandarpdangi@gmail.com>
2021-07-16 18:15:04 +0530

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import arraywords from '../utils/words';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  limit = 10;
+
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+    // console.log(newLimit);
+  }
+  generate() {
+    this.words = arraywords.slice(0, this.limit).join(' ');
+  }
 }
